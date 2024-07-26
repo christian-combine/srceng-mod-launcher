11bd39e7754f2c1b4fc705c8b406eb62a8ca82d1
ItzVlλdik <61982094+ItzVladik@users.noreply.github.com>
2024-07-26 09:00:04 +0500

    Update script.sh

--- android/script.sh
+++ android/script.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/ItzVladik/extras
 cd extras
-../android/scripts/conv-update.sh nh2.png
+../android/scripts/conv-update.sh $ICON
 sed -i -e "s|MOD_REPLACE_ME|$PACKAGE|g" ../android/src/me/nillerusr/LauncherActivity.java
 sed -i -e "s|MOD_REPLACE_ME|$PACKAGE|g" ../android/AndroidManifest.xml
 sed -i -e "s|APP_NAME|$APPNAME|g" ../android/res/values/updates.xml
