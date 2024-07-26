6240e6180a12c098f53d4ddd980620a53a8872f4
vl <vl@dislave.168.0.13>
2024-07-26 18:47:48 +0500

    finally working

--- android/script.sh
+++ android/script.sh
@@ -1,7 +1,8 @@
 git clone https://github.com/ItzVladik/extras
 cd extras
-chmod +x ../android/scripts/conv-update.sh
-../android/scripts/conv-update.sh $ICON
+chmod +x ../android/scripts/conv.sh
+../android/scripts/conv.sh $ICON
+cp -r ../extras/res ../android
 sed -i -e "s|MOD_REPLACE_ME|$PACKAGE|g" ../android/src/me/nillerusr/LauncherActivity.java
 sed -i -e "s|MOD_REPLACE_ME|$PACKAGE|g" ../android/AndroidManifest.xml
 sed -i -e "s|APP_NAME|$APPNAME|g" ../android/res/values/updates.xml
